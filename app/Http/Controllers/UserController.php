21108bc4a62f07ebdabd248b9d31afd534b0014a
AndreaLN25 <andrealn14@gmail.com>
2024-02-13 11:11:12 +0100

    Refactoring basic auth functionalities

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,10 +9,9 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
     
 
- public function registerUser(Request $request){
+    public function registerUser(Request $request){
         try {
             $this->validate($request, [
                 //'name' => 'required|min:4',
@@ -37,45 +36,48 @@ class UserController extends Controller
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
-}
+    }
 
-public function login(Request $request){
-    try {
-        $user = User::where('email', $request->email)->first();
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json([
-                'message' => 'Incorrect credentials.'
-            ], 422);
+    public function login(Request $request){
+        try {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json(['message' => 'Incorrect credentials.'], 422);
+            }
+
+            $token = $user->createToken('API Token')->accessToken;
+            return response(['message' => 'Hello ' . ($user->name), 'token' => $token, 'user' => $user], 200);
+
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        } 
+    }
+
+    public function logout(){ 
+        $user = Auth::user();
+
+        if ($user) {
+            $user->tokens->each->revoke();
+            return response()->json(['message' => 'Bye' . $user->name], 200);
         }
-        $token = $user->createToken('API Token')->accessToken;
-        return response(['message' => 'User ' . ucfirst($user->name) . ' logged successfully', 'token' => $token, 'user' => $user], 200);
-    } catch (\Exception $e) {
-        return response()->json(['error' => $e->getMessage()], 500);
-    } 
-}
-
-public function logout(){ 
-    $user = Auth::user();
-
-    if ($user) {
-        $user->tokens->each->revoke();
-        return response()->json(['message' => 'Bye!', 'user' => $user->name], 200);
     }
-}
 
-public function updateUser(Request $request, $id){
-    $userId = Auth::user()->id;
-    if ($userId != $id) {
-        return response()->json([
-            'error' => 'Unauthorized',
-            'warning' => 'No permission to update.'
-        ], 403);
+    public function updateUser(Request $request, $id){
+        try {
+            $userId = Auth::user()->id;
+
+            if ($userId != $id) {
+                return response()->json(['error' => 'No permission to update.'], 403);
+            }
+
+            User::where('id', $userId)->update(['name'=>$request->name ? $request->name : 'Anonymous']);
+            return response()->json(['message' => 'User name updated successfully'], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
-    User::where('id', $userId)->update(['name'=>$request->name ? $request->name : 'Anonymous']);
-    return response()->json([
-        'message' => 'User updated successfully',
-        'new name' => $request->name ? $request->name : 'Anonymous'
-    ], 200);
-}
 
 }
